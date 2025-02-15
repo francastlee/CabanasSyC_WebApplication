69b454b3b8ee120af2edde1ef57dde46646dc75a
Franklin Castillo <133930634+francastlee@users.noreply.github.com>
2025-02-15 12:19:57 -0600

    Project Initializr
    The backend and frontend projects were created and dependencies were configured.

---
+++ cabanassyc-backend/src/main/java/com/castleedev/cabanassyc_backend/CabanassycBackendApplication.java
@@ -0,0 +1,13 @@
+package com.castleedev.cabanassyc_backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CabanassycBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CabanassycBackendApplication.class, args);
+	}
+
+}
---
+++ cabanassyc-backend/src/test/java/com/castleedev/cabanassyc_backend/CabanassycBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.castleedev.cabanassyc_backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CabanassycBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
